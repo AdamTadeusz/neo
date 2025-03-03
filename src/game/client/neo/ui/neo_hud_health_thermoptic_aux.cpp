94c1f60303222c0d46f2f4a8483b47d147cf3656
AdamTadeusz <adam.tomaszewski@hotmail.co.uk>
2025-03-03 10:38:50 +0000

    correct hint

--- src/game/client/neo/ui/neo_hud_health_thermoptic_aux.cpp
+++ src/game/client/neo/ui/neo_hud_health_thermoptic_aux.cpp
@@ -101,8 +101,8 @@ void CNEOHud_HTA::DrawBuildInfo() const
 	surface()->DrawPrintText(m_wszBuildInfo, ARRAYSIZE(m_wszBuildInfo) - 1);
 }
 
-ConVar neo_cl_hud_health_as_percentage("neo_cl_hud_health_as_percentage", "0", FCVAR_NONE,
-	"Health display mode. 0 = percent, 1 = hitpoints", true, 0, true, 2);
+ConVar neo_cl_hud_health_as_percentage("neo_cl_hud_health_as_percentage", "1", FCVAR_NONE,
+	"Health display mode", true, 0, true, 1);
 void CNEOHud_HTA::DrawHTA() const
 {
 	auto player = C_NEO_Player::GetLocalNEOPlayer();
